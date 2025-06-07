a6f129ab2454f4fefbf3dab1b92a882a776c51c4
christopherrca <rocharocharocha1995@yahoo.com>
2025-06-07 19:12:00 -0400

    added reset button

--- randomNumber/script.js
+++ randomNumber/script.js
@@ -1,18 +1,16 @@
 // psuedo code generate button add event listener insert math.random
 const display = document.querySelector("#display")
 const generateRandom = document.querySelector("#generateBtn")
+const resetBtn = document.querySelector("#resetBtn");
 
-// function generateRandomNumber() {
-//     generateRandom.addEventListener("click", () => {
-//           const randomNumber = Math.floor(Math.random() * 100) + 1;
-//             // Step 3b: Display it
-//           display.textContent = randomNumber;
-
-//     })
-// }
 
 // Step 2: Add event listener directly
 generateRandom.addEventListener("click", () => {
     const randomNumber = Math.floor(Math.random() * 100) + 1;
     display.textContent = randomNumber;
 });
+
+
+resetBtn.addEventListener("click",() => {
+    display.textContent = 0;
+})
