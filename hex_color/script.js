1ebf3180b7aa0da1c52dff1f142cb298dd7f75e3
christopherrca <rocharocharocha1995@yahoo.com>
2025-06-09 15:24:53 -0400

    generate color function step 1

---
+++ hex_color/script.js
@@ -0,0 +1,7 @@
+const hex = document.querySelector(".hex")
+const btn = document.querySelector(".generate")
+
+const generateColor = () => {
+    const randomColor = Math.random().toString(16).substring(2, 8)
+    
+}
