e493b3a7ae81cd86abfd81e8c3516bfbf8af9666
christopherrca <rocharocharocha1995@yahoo.com>
2025-06-10 16:10:27 -0400

    finished the palindrome js app

--- palindrome/index.js
+++ palindrome/index.js
@@ -7,9 +7,17 @@ function palindrome(){
     const word = document.querySelector(".input-text").value;
     const len = word.length;
 
-    let start = word.substring(0, Math.floor(len/2)).toLowerCase;
+    let start = word.substring(0, Math.floor(len/2)).toLowerCase();
 
-    let end = word.substring(len - Math.floor(len/2)).toLowerCase;
+    let end = word.substring(len - Math.floor(len/2)).toLowerCase();
+
+    let flip = end.split("").reverse().join("")
+
+    if(start === flip){
+        result.innerHTML = `${word.toUpperCase()} is a palindrome`
+    } else {
+        result.innerHTML = `${word.toUpperCase()} is NOT a palindrome`
+    }
 
 
 }
