57c442b331858a3a4c573d681b5f4f197d371408
christopherrca <rocharocharocha1995@yahoo.com>
2025-06-13 02:40:49 -0400

    retrieved data into browser html

--- api/ajax.js
+++ api/ajax.js
@@ -14,7 +14,27 @@ function getUsers(e) {
     http.open("GET", "users.json", true);
     http.onload = function () {
         if(this.status === 200) {
-            console.log(this.responseText)
+            // console.log(this.responseText)
+            const users = JSON.parse(this.responseText)
+
+            let output = "";
+
+            users.forEach(function(user) {
+                output += `
+                <hr>
+                <ul>
+                    <li>ID: ${user.id}</li>
+                    <li>Name: ${user.name}</li>
+                    <li>AGE: ${user.age}</li>
+                    <li>Email: ${user.email}</li>
+
+                </ul>
+
+                `
+            })
+
+
+            document.querySelector("#users").innerHTML = output
         }
     }
 
