70229efacc2400a4518d7e5446bd8851288bf512
christopherrca <rocharocharocha1995@yahoo.com>
2025-06-13 02:32:18 -0400

    retreived json data success

--- api/ajax.js
+++ api/ajax.js
@@ -6,3 +6,19 @@ btn.addEventListener("click", getUsers)
 
 // CREATE Function Get Users
 
+function getUsers(e) {
+    e.preventDefault()
+    // console.log("hello")
+
+    const http = new XMLHttpRequest();
+    http.open("GET", "users.json", true);
+    http.onload = function () {
+        if(this.status === 200) {
+            console.log(this.responseText)
+        }
+    }
+
+    http.send()
+
+
+}
